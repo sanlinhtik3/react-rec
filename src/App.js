17a8309362735f1098256a9ac6bae564deced43f
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-07-17 12:14:19 +0630

    SCSS Easy

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const MyContext = React.createContext("Hello React");
+const App = props => {
+  useEffect(()=>{
+    const btn = document.getElementById("btn")
+    btn.addEventListener('click', _ => {
+      console.log('Hello JS')
+    })
+  }, [])
 
-// const App = props => {
-//   return <Navbar/>
-// }
-
-// const Navbar = props => {
-//   return <Brand/>
-// }
-
-// const Brand = props => {
-//   const value = React.useContext(MyContext)
-//   return <h1>{value}</h1>
-// }
-
-class App extends React.Component {
-  render() {
-    return <Navbar/>
-  }
-}
-
-class Navbar extends React.Component {
-  render() {
-    return <Brand/>
-  }
-}
-
-class Brand extends React.Component {
-  static contextType = MyContext;
-  
-  render() {
-    return <h1>{this.context}</h1>
-  }
+  return <button id="btn">Click Me</button>
 }
 
 export default App;
