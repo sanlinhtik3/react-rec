1b9075448c503a9ed8efd6ac494f05cbfc266bf0
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-07-26 18:07:25 +0630

    Fontawesome

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
 const App = props => {
-
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    getProducts()
-    // axios.get('https://dummyjson.com/products').then((res) => console.log(res.data.products[0]))
-    // axios.get('https://dummyjson.com/products').then((res) => setProducts(res.data.products))
-  }, [])
-
-  async function getProducts() {
-    const res = await axios.get('https://dummyjson.com/products');
-    console.log(res)
-    setProducts(res.data.products)
-  }
-
   return (
     <div>
-      {products.map((product) => <h1 key={product.id}> {product.id} {product.title}</h1>)}
+      <i class="fa-solid fa-house-user"></i>
+      <i class="fa-solid fa-house-crack"></i>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
